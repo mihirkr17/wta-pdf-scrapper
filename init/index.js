9b7bdbe7a8c86fcdb1dc593c0387d7995ecebd11
mroy35034 <mroy35034@gmail.com>
2024-09-27 15:10:27 +0600

    sugyrghqdfyffghfghfhdfgfgheriu43535345fghdfgh54bhbdfg

--- init/index.js
+++ init/index.js
@@ -214,7 +214,7 @@ async function init(note) {
                      const slug = slugMaker(title);
 
 
-                     const metaTitle = resource?.tpMetaTitle ? resource?.tpMetaTitle.replace("#eventName", plainEventName)
+                     const metaTitle = templates?.tpMetaTitle ? templates?.tpMetaTitle.replace("#eventName", plainEventName)
                      ?.replace("#player1Surname", player1Surname)
                      ?.replace("#player2Surname", player2Surname)
                      ?.replace("#eventYear", eventYear) : title;
